a4357df51418e71822829455496b2fe5934a7792
Erick Ochieng <erickochieng766@gmail.com>
2023-01-17 18:23:01 +0300

    Comment section

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import Header from './components/Header';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import Blogs from './components/blogs/Blogs';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [blogs, setBlogs] = useState([]);
+
   function showBlogForm(event) {
     const postform = document.querySelector(".post-form")
     postform.classList.add("zoom-out")
     postform.classList.remove("zoom-in")
     postform.style.display = "block"
   }
+
+  useEffect(() => {
+    fetch('http://localhost:4000/blogs')
+    .then(response => response.json())
+    .then(data => setBlogs(data))
+    }, [])
   return (
     <div className="App">
         <div className='name'>
@@ -19,7 +29,7 @@ function App() {
         <Navbar />
 
         <div className="blog" onClick={showBlogForm}>POST</div>
-        <Blogs />
+        <Blogs blogs={blogs} />
 
         <Footer />
     </div>
--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,20 +1,21 @@
-import React from "react"
+import React, {useState} from "react"
 import "./Blogs.css"
-import subaru from "../../assets/posts/subaru-impreza.png"
 import telegram from "../../assets/iconsHv/telegram.png"
 import arrowLeft from "../../assets/iconsHv/x-mark.png"
 import arrowup from "../../assets/iconsFg/arrow-up.png"
 import arrowdown from "../../assets/iconsFg/arrow-down.png"
 
-function Blogs() {
+function Blogs({blogs}) {
     return (
         <div className="blogs-wrapper">
             <h2>Blogs</h2>
             <PostForm send={telegram} />
             <div className="blogs">
-                <Post author="Erick Ochieng" time={Date()} image={subaru} info={"Nothing drives like a Subaru, because nothing else is built like one. Each Subaru is engineered to put your mind at ease, while giving you the power to deeply engage with your life."} />
-                <Post author="Toms Hardware" time={Date()} image={"https://cdn.mos.cms.futurecdn.net/kVCu83sCoAvp5wPGisLotj-970-80.jpg.webp"} info={"AMD's $429 twelve-core Ryzen 9 7900, $329 eight-core Ryzen 7 7700, and $229 six-core Ryzen 5 7600 come to market as the first 'Non-X' Ryzen models for the company's new AM5 platform for Zen 4 processors. As we've seen with AMD's previous-gen lower-power models, these 65W models offer the same core counts and cache as their full-fledged X-series counterparts but come with lower clock speeds and a lower price point."} />
-                <Post author="Pixa May" time={Date()} image={"https://cdn.pixabay.com/photo/2019/09/08/20/54/elephant-4461911_960_720.jpg"} info={"Savanna elephants (Loxodonta africana) are intelligent, sociable, affectionate animals. They portray behaviors similar to humans such as caring for weaker individuals, adoption of orphaned calves and grieving over dead companions. Living in family groups of varying sizes and led by matriarchs, elephants traverse vast landscapes in search of food and water, paying no attention to political borders. With a single mature individual consuming between 250 and 350 kg of vegetation and requiring 110 to 190 litres of water per day, the home ranges of these elephants can span several hundred square kilometres. As a result, elephants play an important role in the modification of ecosystems and creation of conditions suitable for the survival of some plants and animals. They maintain grasslands by reducing tree cover and create water ponds/ wallows for other wildlife as they dig for water using their trunks and tusks. They help with the dispersal and germination of tree species such as Borassus palms and Balanitis aegyptiaca trees, which are native to much of Africa."} />
+                {
+                    blogs.map(blog => {
+                      return <Post key={blog.id} blogpost={blog}/>  
+                    })
+                }
             </div>
         </div>
     );
@@ -45,34 +46,27 @@ function PostForm({send}) {
 }
 
 
-function Post({author, time, image, info}) {
-    function collapseComments(event) {
-        const comments = event.target.closest("section")
-        if(comments.getAttribute("id") === "up") {
-            comments.setAttribute("id","down")
-            comments.style.height = "max-content"
-            comments.firstElementChild.firstElementChild.setAttribute("src", arrowdown)
-        } else {
-            comments.setAttribute("id","up")
-            comments.style.height = "32px"
-            comments.firstElementChild.firstElementChild.setAttribute("src", arrowup)
-        }
-    }
+function Post({blogpost: {author, date, image, description, comments}}) {
+
+    const [collapse, setcollapse] = useState(false);
+
     return (
         <div className="posts">
-            <h3 className="author">{author}</h3>
-            <div className="time">{time}</div>
+            <h3 className="author">{author} | <span className="time">{date}</span></h3>
             <div className="post-content">
                 <Pic url={image} />
-                <p className="about-post">{info}</p>
+                <p className="about-post">{description}</p>
             </div>
-            <section className="comments" id="up">
-                <div className="expand-comments" onClick={collapseComments}><img src={arrowup} alt="collapse comments"/></div>
-                <h5 className="h5" >COMMENTS</h5>
-                <Comment />
-                <Comment />
-                <Comment />
-                <Comment />
+            <section className="comments">
+                <div className="expand-comments" onClick={() => {
+                    setcollapse(!collapse)
+                }}><img src={ collapse ? arrowup : arrowdown} alt="collapse comments"/></div>
+                <h5 className="h5" >Comments</h5>
+                {
+                    collapse ? null : comments.map((comment) => {
+                        return <Comment key={comment.id} comment={comment} />
+                    })
+                }
             </section>
             <CommentForm send={telegram}/>
         </div>
@@ -87,11 +81,16 @@ function Pic({url}) {
     );
 }
 
-function Comment() {
+function Comment({comment: {name, email, body}}) {
     return (
-        <div className="comment">
-            COMMENT
-        </div>
+            <fieldset className="comment">
+                <legend className="comment-legend">
+                    <img className="comment-avatar" src="https://cdn.pixabay.com/photo/2018/08/26/16/23/atm-3632623_960_720.png" alt={email} />
+                    <span className="email-commment">{name}</span>
+                </legend>
+                <div className="comment-body">{body}</div>
+                <div className="comment-email"><a href={`mailto:${email}`}>{email}</a></div>
+            </fieldset>
     );
 }
 
